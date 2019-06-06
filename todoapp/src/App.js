6332596ec5b88db2e2d4d6b44a3650fe40061fef
febanda01 <febanda91@gmail.com>
2019-06-05 20:45:10 -0500

    app-completed

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import ToDos from './ToDos'
-
+import AddToDo from './AddToDo'
 
 
 
@@ -14,14 +14,28 @@ class App extends Component {
   }
 
   deleteTodo = (id) => {
-    console.log(id)
-  }
+   const todos = this.state.todos.filter(todo => {
+     return todo.id !== id 
+   });
+   this.setState({
+     todos: todos
+  })
+}
   
+  addToDo = (todo) => {
+    todo.id = Math.random()
+    let todos = [...this.state.todos, todo]
+    this.setState({
+      todos: todos 
+    })
+  }
+
   render() {
     return (
       <div className="todo-app container">
         <h1 className="center blue-text">Todo's</h1>
         <ToDos todos={this.state.todos} deleteToDo={this.deleteTodo}/>
+        <AddToDo addToDo={this.addToDo}/>
       </div>
     );
   }
