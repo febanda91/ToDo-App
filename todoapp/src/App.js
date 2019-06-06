36095446c28d9244b8a529de15ce2d09677dc801
febanda01 <febanda91@gmail.com>
2019-06-05 21:01:38 -0500

    updated

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -33,7 +33,7 @@ class App extends Component {
   render() {
     return (
       <div className="todo-app container">
-        <h1 className="center blue-text">Todo's</h1>
+        <h1 className="center blue-text">Francisco's To-Do List</h1>
         <ToDos todos={this.state.todos} deleteToDo={this.deleteTodo}/>
         <AddToDo addToDo={this.addToDo}/>
       </div>
