798ec00f92a99e42390a5eebe67bb650224c940a
febanda01 <febanda91@gmail.com>
2019-06-05 19:38:37 -0500

    give-props

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
+import ToDos from './ToDos'
 
 
 class App extends Component {
   state = {
     todos: [
-      {id:1, content: 'buy some milk'}
+      {id:1, content: 'buy some milk'},
+      {id:2, content: 'take out the trash'}
     ]
   }
   render() {
     return (
       <div className="App">
-        
+        <h1 className="center blue-text">Todo's</h1>
+        <ToDos todos={this.state.todos}/>
       </div>
     );
   }
